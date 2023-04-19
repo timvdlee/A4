ebdb8028b0fab5468f23906a1bce14052f4a5962
Tim <s1126607@student.hsleiden.nl>
2023-04-19 18:11:01 +0200

    project page todo style

--- Project_A4/ProjectApp/views.py
+++ Project_A4/ProjectApp/views.py
@@ -10,7 +10,15 @@ def home(request):
 
 
 def project_pagina(request):
-    return render(request, 'project-pagina.html')
+    todos = [
+        {
+            "name": "test123",
+            "deadline": "20-04-2013 16:17",
+            "subtodos": [{"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at felis vitae nunc euismod molestie non et ligula. Nullam tempus, tortor eu fringilla maximus, nibh augue vehicula erat, at condimentum sapien lorem non sem. Sed fermentum hendrerit pretium. Nullam faucibus risus ac elit interdum, vel rutrum turpis rhoncus. Curabitur eget nibh in arcu auctor gravida. Phasellus porta ante non dolor scelerisque porttitor. Integer at egestas tortor. Vivamus pellentesque vehicula sagittis."} for _ in range(6)]
+        } for _ in range(10)
+    ]
+    
+    return render(request, 'project-pagina.html',{"todos":todos})
 
 
 def project_toevoegen(request):
