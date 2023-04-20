afb5133e3ed917e55a950ed7863a1aff3abf8304
Tim <s1126607@student.hsleiden.nl>
2023-04-20 15:33:14 +0200

    Finisht todo's frontend gemaakt
    Co-authored-by: AntiCakejesCult <72666992+AntiCakejesCult@users.noreply.github.com>

--- Project_A4/ProjectApp/views.py
+++ Project_A4/ProjectApp/views.py
@@ -12,13 +12,14 @@ def home(request):
 def project_pagina(request):
     todos = [
         {
-            "name": "test123",
+            "name": "Maken van loginpagina + register page",
             "deadline": "20-04-2013 16:17",
             "subtodos": [{"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at felis vitae nunc euismod molestie non et ligula. Nullam tempus, tortor eu fringilla maximus, nibh augue vehicula erat, at condimentum sapien lorem non sem. Sed fermentum hendrerit pretium. Nullam faucibus risus ac elit interdum, vel rutrum turpis rhoncus. Curabitur eget nibh in arcu auctor gravida. Phasellus porta ante non dolor scelerisque porttitor. Integer at egestas tortor. Vivamus pellentesque vehicula sagittis."} for _ in range(10)]
         } for _ in range(10)
     ]
     
-    return render(request, 'project.html',{"todos":todos})
+    finished_todos = [{"name":"Maken van een maken van een weet ik wel abcdefghijklmnopqrstuvwxyz","id":5} for _ in range(50)]
+    return render(request, 'project.html',{"todos":todos,"finished_todos":finished_todos})
 
 
 def project_toevoegen(request):
