c452f911e726a8c416f46fde0267f2c67ba1afff
Tim <s1126607@student.hsleiden.nl>
2023-04-20 09:35:31 +0200

    removed header for projectpage

--- Project_A4/ProjectApp/views.py
+++ Project_A4/ProjectApp/views.py
@@ -14,7 +14,7 @@ def project_pagina(request):
         {
             "name": "test123",
             "deadline": "20-04-2013 16:17",
-            "subtodos": [{"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at felis vitae nunc euismod molestie non et ligula. Nullam tempus, tortor eu fringilla maximus, nibh augue vehicula erat, at condimentum sapien lorem non sem. Sed fermentum hendrerit pretium. Nullam faucibus risus ac elit interdum, vel rutrum turpis rhoncus. Curabitur eget nibh in arcu auctor gravida. Phasellus porta ante non dolor scelerisque porttitor. Integer at egestas tortor. Vivamus pellentesque vehicula sagittis."} for _ in range(6)]
+            "subtodos": [{"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at felis vitae nunc euismod molestie non et ligula. Nullam tempus, tortor eu fringilla maximus, nibh augue vehicula erat, at condimentum sapien lorem non sem. Sed fermentum hendrerit pretium. Nullam faucibus risus ac elit interdum, vel rutrum turpis rhoncus. Curabitur eget nibh in arcu auctor gravida. Phasellus porta ante non dolor scelerisque porttitor. Integer at egestas tortor. Vivamus pellentesque vehicula sagittis."} for _ in range(10)]
         } for _ in range(10)
     ]
     
