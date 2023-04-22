faad7e3593a4bcfccdbf45471e19d8deb273334a
AntiCakejesCult <72666992+AntiCakejesCult@users.noreply.github.com>
2023-04-22 09:16:00 +0200

    modals gemaakt voor project pagina.

--- Project_A4/ProjectApp/views.py
+++ Project_A4/ProjectApp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .forms import ProjectNaam, TodoForm
 # Create your views here.
 
 
@@ -19,7 +19,16 @@ def project_pagina(request):
     ]
     
     finished_todos = [{"name":"Maken van een maken van een weet ik wel abcdefghijklmnopqrstuvwxyz","id":5} for _ in range(50)]
-    return render(request, 'project.html',{"todos":todos,"finished_todos":finished_todos})
+    project_name = ProjectNaam()
+    todoform = TodoForm()
+    return render(request, 'project.html',
+                  {
+                      "todos":todos,
+                      "finished_todos":finished_todos,
+                      "project_name": project_name,
+                      "todoform": todoform
+                   }
+                  )
 
 
 def project_toevoegen(request):
