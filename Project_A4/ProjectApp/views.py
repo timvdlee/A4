3ed93dce4e4d197a97a1cfe802db95d9ecff6977
AntiCakejesCult <72666992+AntiCakejesCult@users.noreply.github.com>
2023-04-23 13:03:25 +0200

    Test met todoform voor elke todo, genereert steeds een todoform voor elke todo zodat huidige info geparst kan worden.

--- Project_A4/ProjectApp/views.py
+++ Project_A4/ProjectApp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .forms import ProjectNaam, TodoForm
+import uuid
+import random
+from datetime import datetime, timedelta
 # Create your views here.
 
 
@@ -8,25 +11,69 @@ from .forms import ProjectNaam, TodoForm
 def home(request):
     return render(request, 'home.html')
 
+def random_deadline(start_date, end_date):
+    delta = end_date - start_date
+    random_days = random.randrange(delta.days)
+    random_time = random.randrange(24 * 3600)
+    return start_date + timedelta(days=random_days, seconds=random_time)
+
 
 def project_pagina(request):
+    start_date = datetime(2023, 4, 24)
+    end_date = datetime(2023, 12, 31)
+
     todos = [
         {
-            "name": "Maken van loginpagina + register page",
-            "deadline": "20-04-2013 16:17",
-            "subtodos": [{"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at felis vitae nunc euismod molestie non et ligula. Nullam tempus, tortor eu fringilla maximus, nibh augue vehicula erat, at condimentum sapien lorem non sem. Sed fermentum hendrerit pretium. Nullam faucibus risus ac elit interdum, vel rutrum turpis rhoncus. Curabitur eget nibh in arcu auctor gravida. Phasellus porta ante non dolor scelerisque porttitor. Integer at egestas tortor. Vivamus pellentesque vehicula sagittis."} for _ in range(10)]
-        } for _ in range(10)
+            "name": f"Todo {str(uuid.uuid4())}",
+            "deadline": (deadline := random_deadline(start_date, end_date)).strftime("%d-%m-%Y %H:%M"),
+            "deadline-date": deadline.strftime("%Y-%m-%d"),
+            "deadline_time": deadline.strftime("%H:%M"),
+            "id": _,
+            "subtodos": [
+                {
+                    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+                }
+                for _ in range(10)
+            ],
+        }
+        for _ in range(36)
     ]
+    todo_forms = [
+        {
+            'todo': todo,
+            'form': TodoForm(initial={
+                'name': todo['name'],
+                'deadline_date': todo['deadline-date'],
+                'deadline_time': todo['deadline_time'],
+                })
+            }
+        for todo in todos
+        ]
+
+    project_name = "Project"
     
-    finished_todos = [{"name":"Maken van een maken van een weet ik wel abcdefghijklmnopqrstuvwxyz","id":5} for _ in range(50)]
-    project_name = ProjectNaam()
-    todoform = TodoForm()
+    finished_todos = [
+        {
+            "name": "Maken van een maken van een weet ik wel abcdefghijklmnopqrstuvwxyz",
+            "id": 5
+            } for _ in range(50)
+        ]
+    project_name_form = ProjectNaam(
+        initial={
+            'name': project_name
+        }
+    )
+    
+    add_todo = TodoForm()
+
     return render(request, 'project.html',
                   {
-                      "todos":todos,
-                      "finished_todos":finished_todos,
-                      "project_name": project_name,
-                      "todoform": todoform
+                      "todos": todos,
+                      "finished_todos": finished_todos,
+                      "project_name_form": project_name_form,
+                      "todo_form": todo_forms,
+                      "project_naam": project_name,
+                      "add_todo": add_todo,
                    }
                   )
 
