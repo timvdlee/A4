ad87336c2ba5bfbb070a492aa5dacd78a563ab8f
isema <s1130117@student.hsleiden.nl>
2023-04-28 14:03:46 +0200

    login fixed

--- Project_A4/ProjectApp/context_processors.py
+++ Project_A4/ProjectApp/context_processors.py
@@ -1,9 +1,12 @@
 from .models import Project, User
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
 
-
-@login_required
 def dropdown_navbar(request):
-    projects_with_user = Project.objects.filter(
-        members=User.objects.get(id=request.user.id)).order_by('creation_date')
-    return {'dropdown_navbar': projects_with_user}
+    try:
+        projects_with_user = Project.objects.filter(
+            members=User.objects.get(id=request.user.id)).order_by('creation_date')
+        return {'dropdown_navbar': projects_with_user}
+
+    except ObjectDoesNotExist:
+        return ''
