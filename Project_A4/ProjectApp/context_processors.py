09286e0d9335415ccdf89062f44e7de944a7dbae
isema <s1130117@student.hsleiden.nl>
2023-04-28 13:54:11 +0200

    styling homebutton en dropdown gemaakt

--- Project_A4/ProjectApp/context_processors.py
+++ Project_A4/ProjectApp/context_processors.py
@@ -1,7 +1,9 @@
 from .models import Project, User
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def dropdown_navbar(request):
     projects_with_user = Project.objects.filter(
         members=User.objects.get(id=request.user.id)).order_by('creation_date')
-    print(projects_with_user)
+    return {'dropdown_navbar': projects_with_user}
