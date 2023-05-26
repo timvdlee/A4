727cc80ee93b51835e36930abd18bb777cb65c04
Tim <s1126607@student.hsleiden.nl>
2023-05-26 18:36:45 +0200

    zoals gevraagd door salah documentatie verwijderd

--- Project_A4/tests/test_register.py
+++ Project_A4/tests/test_register.py
@@ -1,6 +1,3 @@
-"""
-Test geschreven voor register_view door Salah
-"""
 import pytest
 from django.urls import reverse
 from django.contrib.auth.models import AnonymousUser, User
@@ -11,33 +8,13 @@ from ProjectApp.views import home
 from django.test import Client
 
 
-"""
-request factory maakt http-requests voor de test
-"""
-
-
 @pytest.fixture
 def factory():
     return RequestFactory()
 
-
-"""
-Maakt user object voor de test
-"""
-
-
 @pytest.fixture
 def user():
     return mixer.blend(User)
-
-
-"""
-Maakt eerst http request aan en voegt user eraan toe. 
-De test roept dan register_view aan met de HttpRequest en controleert of 
-de response statuscode gelijk is aan 302 (de redirect-statuscode) en of de 
-response-url gelijk is aan '/' (de homepage).
-"""
-
 
 @pytest.mark.django_db
 def test_register_view_register_autenticated_user_returns_statuscode_url(factory, user):
@@ -49,12 +26,6 @@ def test_register_view_register_autenticated_user_returns_statuscode_url(factory
     actual_status_code = response.status_code
     assert actual_status_code == expect_status_code
     assert response.url == "/"
-
-
-"""
-checkt of de POST-request wordt geaccepteerd en er een redirect plaatsvindt.
-"""
-
 
 @pytest.mark.django_db
 def test_register_view_post_login_registered_user_returns_statuscode(factory):
